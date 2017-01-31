2324a8ec92360b0d15d18ac87f436ec531d70dee
lauret_s <lauret_s@epitech.eu>
2017-01-31 23:51:02 +0100

    add Date class

---
+++ src/Date.cpp
@@ -0,0 +1,47 @@
+#include "Date.hpp"
+
+Date::Date(std::string &date) {
+  std::string str;
+  std::size_t begin = 0, end = 0;
+
+  if ((end = date.find("/", begin)) != std::string::npos) {
+    str = date.substr(begin, end - begin);
+    _day = std::stoi(str);
+    begin = end + 1;
+  }
+  if ((end = date.find("/", begin)) != std::string::npos) {
+    str = date.substr(begin, end - begin);
+    _month = std::stoi(str);
+    begin = end + 1;
+    str = date.substr(begin, date.size());
+    _year = std::stoi(str);
+  }
+}
+
+Date::Date(int day, int month, int year)
+: _day(day), _month(month), _year(year) {
+
+}
+
+Date::~Date() {
+
+}
+
+std::string Date::toString() {
+  std::stringstream  tmp;
+
+  tmp << _day << "/" << _month << "/" << _year;
+  return tmp.str();
+}
+
+int         Date::getDay() {
+  return _day;
+}
+
+int         Date::getMonth() {
+  return _month;
+}
+
+int         Date::getYear() {
+  return _year;
+}
---
+++ src/Date.hpp
@@ -0,0 +1,25 @@
+#ifndef DATE_HPP
+# define DATE_HPP
+
+#include <sstream>
+#include <string>
+
+class Date {
+public:
+  Date(std::string &date);
+  Date(int,int,int);
+  ~Date();
+
+  std::string toString();
+
+  int         getDay();
+  int         getMonth();
+  int         getYear();
+
+private:
+  int _day;
+  int _month;
+  int _year;
+};
+
+#endif //!DATE_HPP
